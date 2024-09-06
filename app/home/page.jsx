559367f2983e27ccfd9d73d4ff1033851622944d
ikko awinata <ikko.cantik21@gmail.com>
2024-09-06 10:18:35 +0700

    ubah warna navbar

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -47,6 +47,7 @@ import Badge from "@/components/Badge"; // Pastikan jalur ini benar
 import Socials from "@/components/Socials"; // Pastikan jalur ini benar
 import { RiArrowDownSLine } from "react-icons/ri";
 import Image from "next/image";
+import "/app/home/tentang.css";
 
 
 const Hero = () => {
